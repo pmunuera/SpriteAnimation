0b966d6568f604b04f687ecaed9954b653222e53
pmunuera <95918506+pmunuera@users.noreply.github.com>
2023-04-11 16:36:58 +0200

    Initial commit

---
+++ desktop/src/com/pablomunuera/spriteanimation/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.pablomunuera.spriteanimation;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.pablomunuera.spriteanimation.SpriteAnimation;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("SpriteAnimation");
+		new Lwjgl3Application(new SpriteAnimation(), config);
+	}
+}
